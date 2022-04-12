51a164b46a068ebf8c35314963dc2cc01d4a10a0
Panda <panda@PandadeMac-mini.lan>
2022-04-12 19:35:29 +0800

    环境变量备注

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     script: './dist/bot.js',
     watch: './dist',
     env: {
-      BOTTOKEN: ''//机器人Token
+      BOTTOKEN: '',//机器人Token
+      HOSTID:'',//bot所有者ID,包含数据写入权限,须特别注意.
     }
   }],
 };
